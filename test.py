ad0b16a49a34712261d4a2fa4b63e02080372f15
Rafal Owczarski <R.R.Owczarski@student.tudelft.nl>
2025-02-23 16:39:18 +0100

    Initial setup

---
+++ test.py
@@ -0,0 +1,14 @@
+import time
+
+from EnergiBridgeRunner import EnergiBridgeRunner
+
+runner = EnergiBridgeRunner()
+runner.start(results_file="output.csv")
+
+
+# Perform some task
+time.sleep(10)
+
+# Stop the data collection and retrieve results
+energy, duration = runner.stop()
+print(f"Energy consumption (J): {energy}; Execution time (s): {duration}")
