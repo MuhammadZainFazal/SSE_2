31aaf9b1a16ce7b1c7d192f4fdf985428f8cf32e
Rafal Owczarski <R.R.Owczarski@student.tudelft.nl>
2025-03-25 17:25:07 +0100

    Finalize everything for the actual run

--- pipeline.py
+++ pipeline.py
@@ -10,15 +10,15 @@ from scenario.scenario import Scenario
 # Directories for Java files and rulesets
 project_dir = os.path.dirname(os.path.abspath(__file__))
 java_files_directory = [os.path.join(project_dir, 'java_files', 'junit5-r5.12.1')]
-#ruleset_paths = [os.path.join(project_dir, 'rulesets', f) for f in os.listdir(os.path.join(project_dir, 'rulesets')) if f.endswith('.xml')]
-ruleset_paths = [os.path.join(project_dir, 'rulesets\\rulesets\\java\\quickstart.xml', )]
+ruleset_paths = [os.path.join(project_dir, 'rulesets', 'category', 'java', f) for f in os.listdir(os.path.join(project_dir, 'rulesets', 'category', 'java')) if f.endswith('.xml')]
+#ruleset_paths = [os.path.join(project_dir, 'rulesets\\rulesets\\java\\quickstart.xml', )]
 
 
 energibridge_runner = EnergiBridgeRunner()
 print(java_files_directory)
 scenarios = [
     Scenario(
-        name=os.path.basename(file),
+        name=os.path.basename(file) + ':' + os.path.basename(rule),
         description=f"Test {os.path.basename(file)}",
         src_dir=os.path.dirname(file),
         ruleset_path=rule,
@@ -28,7 +28,7 @@ scenarios = [
     for rule in ruleset_paths
 ]
 
-runner = Runner(scenarios, number_of_runs=6)
+runner = Runner(scenarios, number_of_runs=10)
 runner.run()
 
 for scenario in scenarios:
